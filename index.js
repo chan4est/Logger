3bca2c9dcf137af7e48a3097132238a969c88869
Chandler Forrest <chan4est@gmail.com>
2018-12-03 23:07:56 -0800

    no dynamic port

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ function start(repo) {
     })
   })
 }
-
-app.listen(3000)
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 // host deploy script
